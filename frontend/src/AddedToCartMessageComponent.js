237d6e9f544a8b86bc4b328ceba7d3b302142300
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-03-11 09:57:14 +0530

    v0.0.2 Modifying product detail pages

---
+++ frontend/src/AddedToCartMessageComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
+import {Link} from "react-router-dom"
+
+const AddedToCartMessageComponent = () => {
+  const [show, setShow] = useState(true);
+  return (
+    <Alert
+      show={show}
+      variant="success"
+      onClose={() => setShow(false)}
+      dismissible
+    >
+      <Alert.Heading>The product was added to your cart!</Alert.Heading>
+      <p>
+        <Button variant="success">Go Back</Button>{" "}
+        <Link to="/cart">
+          <Button variant="danger">Go to Cart</Button>
+        </Link>
+      </p>
+    </Alert>
+  );
+};
+
+export default AddedToCartMessageComponent;
--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,26 +1,99 @@
-import { Col, Container, Row } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
+import AddedToCartMessageComponent from "../AddedToCartMessageComponent";
 
 const ProductDetailsPage = () => {
   return (
-    <Container  >
+    <Container>
+      <AddedToCartMessageComponent />
       <Row className="mt-5">
-        <Col md={4} >
-          Images
+        <Col md={4}>
+          <Image fluid src="/images/games-category.png" />
+          <Image fluid src="/images/monitors-category.png" />
+          <Image fluid src="/images/tablets-category.png" />
+          <Image fluid src="/images/games-category.png" />
         </Col>
         <Col md={8}>
           <Row>
-            <Col md={8} >
-              Product name, Price, description, rating
+            <Col md={8}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h1>Product Name</h1>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating readonly size={20} initialValue={4} />
+                  (1)
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Price: <span className="fw-bold">$232</span>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Porta ac consectetur ac Porta ac consectetur ac Porta ac
+                  consectetur ac Porta ac consectetur a cPorta ac consectetur
+                  ac Porta ac consectetur ac Porta ac consectetur ac
+                </ListGroup.Item>
+              </ListGroup>
             </Col>
-            <Col md={4} >
-              Product status, quantity
+            <Col md={4}>
+              <ListGroup>
+                <ListGroup.Item>Status: in Stock</ListGroup.Item>
+                <ListGroup.Item> Price: <span className="fw-bold">$232</span></ListGroup.Item>
+                <ListGroup.Item>
+                  <Form.Select size="lg" aria-label="Default select example">
+                    <option>Quantity: </option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </Form.Select>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button variant="danger">Add to Cart</Button>
+                </ListGroup.Item>
+              </ListGroup>
             </Col>
           </Row>
           <Row className="mt-5">
             <h5>REVIEWS</h5>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
           </Row>
           <hr />
           Send Review Form
+          <Alert variant="danger">Login first to write a review</Alert>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Form.Select aria-label="Default select example">
+              <option>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+            <Button variant="warning">Button</Button>
+          </Form>
         </Col>
       </Row>
     </Container>
