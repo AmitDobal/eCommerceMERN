46fbd2e3e87b000c4055a651d91b927a50c86d86
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-03-12 22:03:28 +0530

    v0.0.5 - Login page UI done

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,82 @@
+import { useState } from "react";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  Row,
+  Spinner,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <p>This is a login page</p>;
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+  return (
+    <Container>
+      <Row className="mt-5 justify-content-md-center">
+        <Col md={6}>
+          <h1>Login</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                required
+                type="email"
+                placeholder="Enter Email"
+                name="email"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                name="doNotLogout"
+                label="Do not logout"
+              />
+            </Form.Group>
+
+            <Row className="pb-2">
+              <Col>
+                Don't you have an account?
+                <Link to={"/register"}> Register </Link>
+              </Col>
+            </Row>
+            <Button variant="primary" type="submit" className="mb-3">
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Login
+            </Button>
+            <Alert show={true} variant="danger">
+              Wrong credentials!
+            </Alert>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default LoginPage;
-
