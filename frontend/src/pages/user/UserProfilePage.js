74c50a242092fa1201c8d18550735bf7c65fbed8
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-03-12 22:27:43 +0530

    v0.0.5 - User Profile UI complete

--- frontend/src/pages/user/UserProfilePage.js
+++ frontend/src/pages/user/UserProfilePage.js
@@ -1,6 +1,161 @@
+import { useState } from "react";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
+
 const UserProfilePage = () => {
-  return <p>This is a user profile page</p>;
+  const [validated, setValidated] = useState(false);
+
+  const onChange = (e) => {
+    const password = document.querySelector("input[name=password]");
+    const confirm = document.querySelector("input[name=confirmPassword]");
+    console.log("Password:", password.value);
+    console.log("Confirm:", confirm.value);
+    if (password.value === confirm.value) {
+      confirm.setCustomValidity("");
+    } else {
+      confirm.setCustomValidity("Password do not match!");
+    }
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+  return (
+    <Container>
+      <Row className="mt-5 justify-content-md-center">
+        <Col md={6}>
+          <h1>User Profile</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="validationCustom01">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                defaultValue={"John"}
+                name="name"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please Enter a Correct Name!
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                defaultValue={"Doe"}
+                name="lastName"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please Enter a Correct Name!
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control disabled value="john@doe.com" />
+              <Form.Text className="text-muted">
+                If you want to change email, remove account and create new one
+                with new email address
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPhone">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your phone number"
+                defaultValue={""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicAddress">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your street name and house number"
+                defaultValue={""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCountry">
+              <Form.Label>Country</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your country"
+                defaultValue={""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicZip">
+              <Form.Label>Zip Code</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your Zip code"
+                defaultValue={""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCity">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your City"
+                defaultValue={""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicState">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your State"
+                defaultValue={""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                name="password"
+                minLength={6}
+                onChange={onChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter valid password!!
+              </Form.Control.Feedback>
+              <Form.Text className="text-muted">
+                Password should have atleast 6 characters
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Repeat Password"
+                name="confirmPassword"
+                minLength={6}
+                onChange={onChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Both password should match!!
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mb-3">
+              Update
+            </Button>
+            <Alert show={true} variant="danger">
+              User with that email already exist!
+            </Alert>
+            <Alert show={true} variant="info">
+              User Updated
+            </Alert>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default UserProfilePage;
-
