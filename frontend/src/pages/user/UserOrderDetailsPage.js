04de0894be13a0f549f5dad9631949400c1c7751
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-03-13 15:54:43 +0530

    v0.0.6 - user orders and order details page UI completed

--- frontend/src/pages/user/UserOrderDetailsPage.js
+++ frontend/src/pages/user/UserOrderDetailsPage.js
@@ -1,6 +1,86 @@
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
+import CartItemComponent from "../../components/CartItemComponent";
 const UserOrderDetailsPage = () => {
-  return <p>This is a user order details page</p>;
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <h1>Order Details</h1>
+        <Col md={8}>
+          <br />
+          <Row>
+            <Col md={6}>
+              <h2>Shipping</h2>
+              <b>Name</b>: John Doe <br />
+              <b>Address</b>: Galaxy IT park, sector 62, Noida, UP 110011 <br />
+              <b>Phone</b>: +91-123123123123 <br />
+            </Col>
+            <Col md={6}>
+              <h2>Payment Method</h2>
+              <Form.Select disabled={false}>
+                <option value={"pp"}>PayPal</option>
+                <option value={"cod"}>
+                  Cash On Delivery(Delivery may be delayed)
+                </option>
+              </Form.Select>
+            </Col>
+            <Row>
+              <Col>
+                <Alert className="mt-3" variant="danger">
+                  Not delivered
+                </Alert>
+              </Col>
+              <Col>
+                <Alert className="mt-3" variant="success">
+                  Paid on 2023-03-13
+                </Alert>
+              </Col>
+            </Row>
+          </Row>
+          <br />
+          <h2>Order Items</h2>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((_, idx) => (
+              <CartItemComponent key={idx} />
+            ))}
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Order Summary</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Items price (after tax): <span className="fw-bold">$100</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Shipping: <span className="fw-bold">included</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Tax: <span className="fw-bold">included</span>
+            </ListGroup.Item>
+            <ListGroup.Item className="text-danger">
+              Total price: <span className="fw-bold ">$100</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <div className="d-grid">
+                <Button size="lg" variant="danger" type="button">
+                  Pay for the order
+                </Button>
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default UserOrderDetailsPage;
-
--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,6 +1,44 @@
+import { Col, Row, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const UserOrdersPage = () => {
-  return <p>This is a user orders page</p>;
+  return (
+    <Row className="m-5">
+      <Col md={12}>
+        <h1>My Orders</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Delivered</th>
+              <th>Order details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
+              (item, idx) => (
+                <tr>
+                  <td>{idx + 1}</td>
+                  <td>Mark Twain</td>
+                  <td>2023-03-13</td>
+                  <td>$124</td>
+                  <td>
+                    <i className={item} />
+                  </td>
+                  <td>
+                    <Link to="/user/order-details">Go to order</Link>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
 };
 
 export default UserOrdersPage;
-
