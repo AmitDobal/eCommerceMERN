77fd5c272ff60a4ae378922e62cbb85e554b7f68
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-03-16 21:08:12 +0530

    v0.0.7 - admin UI page create order and product page UI complete

--- frontend/src/pages/admin/AdminCreateProductPage.js
+++ frontend/src/pages/admin/AdminCreateProductPage.js
@@ -1,6 +1,184 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  CloseButton,
+  Col,
+  Container,
+  Form,
+  Row,
+  Table,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const AdminCreateProductPage = () => {
-  return <p>This is a create product page</p>;
+  const [validated, setValidated] = useState(false);
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+  return (
+    <Container>
+      <Row className="mt-5 justify-content-md-center">
+        <Col md={1}>
+          <Link to="/admin/products" className="btn btn-info my-3">
+            Go Back
+          </Link>
+        </Col>
+        <Col md={6}>
+          <h1>Create a new product</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Product Name"
+                name="name"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                required
+                as="textarea"
+                name="description"
+                rows={3}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCount">
+              <Form.Label>Count in stock</Form.Label>
+              <Form.Control required type="number" name="count" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCount">
+              <Form.Label>Price</Form.Label>
+              <Form.Control required type="number" name="price" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCategory">
+              <Form.Label>
+                Category
+                <CloseButton />(<small>remove selected</small>)
+              </Form.Label>
+              <Form.Select
+                required
+                name="category"
+                aria-label="default select example"
+              >
+                <option value="">Choose category</option>
+                <option value="1">Laptops</option>
+                <option value="2">TV</option>
+                <option value="3">Games</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3 " controlId="formBasicNewCategory">
+              <Form.Label>
+                Or create a new category (e.g. Computers/Laptops/Intel){" "}
+              </Form.Label>
+              <Form.Control name="newCategory" type="text" />
+            </Form.Group>
+            <Row className="mt-5">
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicAttributes">
+                  <Form.Label>Choose attribute and set value</Form.Label>
+                  <Form.Select
+                    name="atrrKey"
+                    aria-label="Default select example"
+                  >
+                    <option value="">Choose attribute</option>
+                    <option value="red">color</option>
+                    <option value="2">TV</option>
+                    <option value="3">Games</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicAttributeValue"
+                >
+                  <Form.Label>Attribute value</Form.Label>
+                  <Form.Select
+                    name="atrrVal"
+                    aria-label="Default select example"
+                  >
+                    <option value="">Choose attribute value</option>
+                    <option value="red">color</option>
+                    <option value="2">TV</option>
+                    <option value="3">Games</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>Attribute</th>
+                    <th>Value</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>attr key</td>
+                    <td>attr value</td>
+                    <td>
+                      <CloseButton />
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicNewAttribute">
+                  <Form.Label>Create new attribute</Form.Label>
+                  <Form.Control
+                    disabled={false}
+                    type="text"
+                    placeholder="First choose or create category"
+                    name="newAttrValue"
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicNewAttributeValue"
+                >
+                  <Form.Label>Attribute value</Form.Label>
+                  <Form.Control
+                    disabled={false}
+                    required={false}
+                    type="text"
+                    placeholder="First choose or create category"
+                    name="newAttrValue"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Alert variant="primary">
+              After typing attribute key value press enter on one of the field
+            </Alert>
+            <Form.Group className="mb-3 mt-3" controlId="formFileMultiple">
+              <Form.Label>Images</Form.Label>
+              <Form.Control required type="file" multiple />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Create
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AdminCreateProductPage;
-
