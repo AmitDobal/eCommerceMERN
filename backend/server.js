095f304d3e029fdc33910f695874863b0f5bc319
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-03-20 23:24:45 +0530

    v0.1.0 - express done and mongo DB connection completed

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const apiRoutes = require("./routes/apiRoutes");
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
+//mongoDB Connection
+const connectDB = require("./config/db");
+connectDB();
+
+app.use("/api", apiRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
