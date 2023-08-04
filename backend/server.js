12fbe3f1eaaae4fe591589030e7fbcb47e9f5439
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-08-05 00:27:39 +0530

    Added endpoints for admin products

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(fileUpload());
 
 const apiRoutes = require("./routes/apiRoutes");
 
