cc5b943e71216192ec2f4dc3fab576c54e1a5ea4
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-08-05 23:42:38 +0530

    user created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,40 @@
 const User = require("../models/UserModel");
-const getUsers = async (req, res) => {
+const { hashPassword } = require("../utils/hashPassword");
+
+//FETCH ALL USERS
+const getUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
+    const users = await User.find().select("-password");
     res.json(users);
   } catch (error) {
-    console.log(error.message);
-    res.json({ err: error, message: error.message });
+    next(error);
   }
 };
 
-module.exports = getUsers;
+//REGISTER USER
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ error: "User Exists (email)" });
+    } else {
+      const hashedPassword = hashPassword(password);
+      const user = await User.create({
+        name,
+        lastName,
+        password: hashedPassword,
+        email: email.toLowerCase(),
+      });
+      res.status(201).json(user);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getUsers, registerUser };
---
+++ backend/utils/hashPassword.js
@@ -0,0 +1,7 @@
+const bcrypt = require("bcryptjs");
+
+const salt = bcrypt.genSaltSync(10);
+
+const hashPassword = (password) => bcrypt.hashSync(password, salt);
+
+module.exports = { hashPassword };
