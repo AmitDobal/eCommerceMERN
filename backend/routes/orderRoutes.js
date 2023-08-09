36e0bc26cab53a90d0957f280394ba645ec3ba8f
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-08-10 00:12:51 +0530

    Order analysis completed. Endpoint creation completed

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const {getUserOrders} = require("../controllers/orderController");
+const {
+  verifyIsLoggedIn,
+  verifyIsAdmin,
+} = require("../middleware/verifyAuthToken");
+const {
+  getUserOrders,
+  getOrder,
+  createOrder,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+  getOrderForAnalysis,
+} = require("../controllers/orderController");
+
+//user Routes
+router.use(verifyIsLoggedIn);
 
 router.get("/", getUserOrders);
+router.get("/user/:id", getOrder);
+router.post("/", createOrder);
+router.put("/paid/:id", updateOrderToPaid);
+
+//Admin Routes
+router.use(verifyIsAdmin);
+router.put("/delivered/:id", updateOrderToDelivered);
+router.get("/admin", getOrders);
+router.get("/analysis/:date", getOrderForAnalysis);
 
 module.exports = router;
