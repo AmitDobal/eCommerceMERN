6323284ba04d38a3a20ac2b295c60a92485b6af9
Amit Singh Dobal <2019pte5188@mnit.ac.in>
2023-08-14 00:20:19 +0530

    redux setup done

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -13,14 +13,24 @@ import AddedToCartMessageComponent from "../components/AddedToCartMessageCompone
 import ImageZoom from "js-image-zoom";
 import { useEffect } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../redux/actions/cartActions";
+
 const ProductDetailsPage = () => {
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(addToCart());
+  };
+
+  const product = useSelector((state) => state.cart.value);
+
   var options = {
     // width: 400,
     // zoomWidth: 500,
     scale: 2,
     offset: { vertical: 0, horizontal: 0 },
   };
-  console.log("Product Details Page");
   useEffect(() => {
     new ImageZoom(document.getElementById("first"), options);
     new ImageZoom(document.getElementById("second"), options);
@@ -86,7 +96,9 @@ const ProductDetailsPage = () => {
                   </Form.Select>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Button variant="danger">Add to Cart</Button>
+                  <Button onClick={addToCartHandler} variant="danger">
+                    Add to Cart
+                  </Button>
                 </ListGroup.Item>
               </ListGroup>
             </Col>
